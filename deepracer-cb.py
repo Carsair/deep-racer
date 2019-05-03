70fcad980c890c6cb46c2a66b830620591999e90
Carsair <Carsair@users.noreply.github.com>
2019-05-02 22:37:29 -0400

    v11 or so.

--- deepracer-cb.py
+++ deepracer-cb.py
@@ -18,13 +18,13 @@
 
 import math
 
-myVal = 10
+position_points = []
 
 def getHeading(x1, y1, x2, y2):
     deltax = (x2 - x1)
     # Make safe for validator
     if deltax == 0:
-        deltax = .0001
+        deltax = 0.0001
 
     degrees = math.degrees(math.atan((y2 - y1) / deltax))
     if degrees > 180:
@@ -36,15 +36,15 @@ def getDistance(x1, y1, x2, y2):
 
 def reward_function(params):
     print('=====START PARAMS')
-    global myVal
-    print('myVal', myVal)
-    myVal = myVal + 10
     print('all_wheels_on_track ' + str(params['all_wheels_on_track']))
     all_wheels_on_track = params['all_wheels_on_track']
     print('x ' + str(params['x']))
     x = params['x']
     print('y ' + str(params['y']))
     y = params['y']
+    global position_points
+    position_points.append([x, y])
+    print('position_points', position_points)
     print('distance_from_center ' + str(params['distance_from_center']))
     distance_from_center = params['distance_from_center']
     print('is_left_of_center ' + str(params['is_left_of_center']))
@@ -68,15 +68,24 @@ def reward_function(params):
     print('=====END PARAMS')
 
     reward = 0
-    bonus = 2 * (track_width - distance_from_center) / track_width
+    bonus = 0
     speedBonus = speed
+    edgeMultiplier = 1
+    if (distance_from_center / (track_width / 2)) > 0.75:
+        edgeMultiplier = 0.5
+    elif (distance_from_center / (track_width / 2)) > 0.9:
+        edgeMultiplier = 0.1
+    elif (distance_from_center / (track_width / 2)) > 0.95:
+        edgeMultiplier = -0.5
     infoBonus = 0
-    infoPoints = waypoints[closest_waypoints[0]: closest_waypoints[0] + 20]
+    infoPoints = waypoints[closest_waypoints[0]: closest_waypoints[0] + 10]
     print('infoPoints: ', infoPoints)
 
     for idx in range(0, len(infoPoints) - 1):
         if infoPoints[idx]:
             multiplier = 1 / ((idx + 1)**1.5)
+            print("multiplier " + str(multiplier))
+
             if infoPoints[idx] and infoPoints[idx + 1]:
                 xwaypoint1 = infoPoints[idx][0]
                 ywaypoint1 = infoPoints[idx][1]
@@ -88,29 +97,17 @@ def reward_function(params):
                 xwaypoint2 = infoPoints[idx][0]
                 ywaypoint2 = infoPoints[idx][1]
 
-            # headingMultiplier = 360
-            headingDiff = getHeading(xwaypoint1, ywaypoint1, xwaypoint2, ywaypoint2) - heading
-            # headingFactor = 10 * (headingMultiplier - abs(heading - headingDiff)) / headingMultiplier
-            headingFactor = headingDiff
+            headingDiff = abs(getHeading(xwaypoint1, ywaypoint1, xwaypoint2, ywaypoint2) - heading)
+            if headingDiff > 180:
+                headingDiff = 360 - headingDiff
+            headingDiff = (180 - headingDiff) / 18
 
-            # distanceMultiplier = 5 * track_width
-            distanceDiff = min(getDistance(x, y, xwaypoint1, ywaypoint1), getDistance(x, y, xwaypoint2, ywaypoint2))
-            # distanceFactor = 10 * (distanceMultiplier - distanceDiff) / distanceMultiplier
-            distanceFactor = 10 * distanceDiff
-
-            # infoSteering = heading + steering_angle
-            # steeringFactor = steering_angle
-
-            print("multiplier " + str(multiplier))
             print("headingDiff " + str(headingDiff))
-            print("headingFactor " + str(headingFactor))
-            print("distanceDiff " + str(distanceDiff))
-            print("distanceFactor " + str(distanceFactor))
-            reward = reward + (multiplier * (headingFactor + distanceFactor))
+            infoBonus = infoBonus + (multiplier * headingDiff)
 
     print("infoBonus " + str(infoBonus))
     print("bonus " + str(bonus))
     print("speedBonus " + str(speedBonus))
-    reward = infoBonus + bonus + speedBonus
+    reward = edgeMultiplier * (infoBonus + bonus + speedBonus)
     print("reward " + str(float(reward)))
     return float(reward)
